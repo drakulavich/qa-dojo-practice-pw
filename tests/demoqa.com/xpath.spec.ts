91d50d8572ed0a372d45fb34fe43aa9e899f75e6
Anton Yakutovich <drakulavich@gmail.com>
2025-08-10 21:24:55 +0400

    HW4 finish initial tests

--- tests/demoqa.com/xpath.spec.ts
+++ tests/demoqa.com/xpath.spec.ts
@@ -19,6 +19,9 @@ const toggleButton = (itemName: string) =>
 const checkBox = (itemName: string) => 
     `//*[text()='${itemName}']/preceding-sibling::span[@class='rct-checkbox']`;
 
+const radioButton = (option: string) =>
+    `//label[text() = '${option}']`;
+
 test.beforeEach(async ({ page }) => {
   await page.route('**/*', route => {
     const url = route.request().url();
@@ -56,31 +59,27 @@ test('check box', async ({ page }) => {
 });
 
 test('radio button', async ({ page }) => {
+  const resultLocator = page.locator("//*[@id='app']//p");
   await page.goto('https://demoqa.com/radio-button');
 
-  await page.locator("//input[@placeholder='Full Name']").fill(USER.fullname);
-  await page.locator("//input[@placeholder='name@example.com']").fill(USER.email);
-  await page.locator("//textarea[@placeholder='Current Address']").fill(USER.currentAddress);
-  await page.locator("//textarea[@id='permanentAddress']").fill(USER.permanentAddress);
-  await page.locator("//button[@id='submit']").click();
+  await page.locator(radioButton('Impressive')).click();
+  await expect(resultLocator).toContainText('You have selected Impressive');
 
-  for (const [key, value] of Object.entries(USER)) {
-    console.log(`Checking ${key} value`);
-    await expect(page.locator("//div[@id='output']")).toContainText(value);
-  };
+  await page.locator(radioButton('Yes')).click();
+  await expect(resultLocator).toContainText('You have selected Yes');
+
+  await expect(page.locator(radioButton('No'))).toBeDisabled();
 });
 
 test('buttons', async ({ page }) => {
   await page.goto('https://demoqa.com/buttons');
 
-  await page.locator("//input[@placeholder='Full Name']").fill(USER.fullname);
-  await page.locator("//input[@placeholder='name@example.com']").fill(USER.email);
-  await page.locator("//textarea[@placeholder='Current Address']").fill(USER.currentAddress);
-  await page.locator("//textarea[@id='permanentAddress']").fill(USER.permanentAddress);
-  await page.locator("//button[@id='submit']").click();
+  await page.locator("//button[text() = 'Double Click Me']").dblclick();
+  await expect(page.locator("//*[@id='doubleClickMessage']")).toContainText('You have done a double click');
 
-  for (const [key, value] of Object.entries(USER)) {
-    console.log(`Checking ${key} value`);
-    await expect(page.locator("//div[@id='output']")).toContainText(value);
-  };
+  await page.locator("//button[text() = 'Right Click Me']").click({ button: 'right' });
+  await expect(page.locator("//*[@id='rightClickMessage']")).toContainText('You have done a right click');
+
+  await page.locator("//button[text() = 'Click Me']").click();
+  await expect(page.locator("//*[@id='dynamicClickMessage']")).toContainText('You have done a dynamic click');
 });
